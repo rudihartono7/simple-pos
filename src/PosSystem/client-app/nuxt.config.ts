f0338fc114c14010d81faf18f02ddf518c181914
rudihartono <rudi.hartono@live.com>
2025-08-09 06:19:17 +0700

    no need define the base url for api

--- src/PosSystem/client-app/nuxt.config.ts
+++ src/PosSystem/client-app/nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5001'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || ''
     }
   },
 
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
   nitro: {
     devProxy: {
       '/api': {
-        target: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5001',
+        target: process.env.NUXT_PUBLIC_API_BASE || '',
         secure: false,
         changeOrigin: true
       }
